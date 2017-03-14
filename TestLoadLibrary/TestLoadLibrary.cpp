1c3df8b222fe812c0adad17644877ec2a754844c
fahadash <fahadash@gmail.com>
2017-03-14 16:48:10 -0400

    modify load library tester to also write output to file

--- TestLoadLibrary/TestLoadLibrary.cpp
+++ TestLoadLibrary/TestLoadLibrary.cpp
@@ -5,32 +5,45 @@
 #include "windows.h"
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <fstream>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
+	stringstream str;
+
 	if (argc < 2)
 	{
-		cout << "USAGE: TestLoadLibrary <path>" << endl;
+		str << "USAGE: TestLoadLibrary <path>" << endl;
 	}
 	else
 	{
 
 		wstring s = wstring(argv[1], argv[1] + strlen(argv[1]));
-		cout << "Loading library " << s.c_str() << "..." << endl;
+		str << "Loading library " << s.c_str() << "..." << endl;
 		auto handle = LoadLibrary(s.c_str());
 
 		if (handle == NULL)
 		{
-			cout << "Failed to load library" << endl;
-			cout << "GetLastError() Returns: " << GetLastError() << endl;
+			str << "Failed to load library" << endl;
+			str << "GetLastError() Returns: " << GetLastError() << endl;
 		}
 		else
 		{
-			cout << "Load Library succeeded";
+			str << "Load Library succeeded";
 			CloseHandle(handle);
 		}
+
 	}
+
+	string fin = str.str();
+
+
+	cout << fin;
+
+	ofstream f("LoadLibraryOutput.txt");
+	f << fin;
     return 0;
 }
 
